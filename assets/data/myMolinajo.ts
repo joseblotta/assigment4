9e0e0562d431160fa0cfa09f3d4ced83dc973e67
joseblotta <52714385+joseblotta@users.noreply.github.com>
2019-08-06 22:01:53 -0400

    Add files via upload

---
+++ assets/data/myMolinajo.ts
@@ -0,0 +1,9 @@
+import { Molinajo } from "../../app/molinajo"
+
+export const MYMOLINAJO: Molinajo = {
+    studentID : 991496251,
+    name : "Jose Molnair Blotta",
+    loginName : "molinajo",
+    campus : "Davis",
+    assignment : "Assignment4"
+}
